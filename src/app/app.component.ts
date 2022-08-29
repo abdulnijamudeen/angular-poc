d51a36bb458c904ce16c55c7bc54e0bde102c38d
Nizam Abdul <abdulnijamudeen@gmail.com>
2022-08-29 08:22:12 +0530

    Added capacitor back button fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { App, BackButtonListenerEvent } from '@capacitor/app';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
@@ -8,7 +9,15 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(private primengConfig: PrimeNGConfig) {
+    App.addListener('backButton', (event: BackButtonListenerEvent) => {
+      if(event.canGoBack) {
+        window.history.back();
+      } else {
+        App.exitApp()
+      }
+    });
+  }
 
   ngOnInit() {
       this.primengConfig.ripple = true;
