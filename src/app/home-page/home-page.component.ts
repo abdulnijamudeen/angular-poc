a7647ef77bd7386a6e84425d3d2f529e21cad4cd
Nizam Abdul <abdulnijamudeen@gmail.com>
2022-09-01 13:16:55 +0530

    Impl Capactitor js Google Auth

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 
 @Component({
   selector: 'app-home-page',
@@ -10,6 +11,21 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  user: any;
+  reason: any;
+  out: any;
+  login() {
+    GoogleAuth.signIn()
+    .then(user => this.user = user)
+    .catch(reason => this.reason = reason);
+  }
+
+  logout() {
+    GoogleAuth.signOut()
+    .then(v => this.out = v)
+    .catch(reason => this.reason = reason);
   }
 
 }
