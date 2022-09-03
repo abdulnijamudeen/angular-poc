5cca206717182377eb204f9f479f44ee75c441d0
Nizam Abdul <abdulnijamudeen@gmail.com>
2022-09-03 09:23:31 +0530

    Impl google capacitor auth with firebase

--- src/app/filebase-google-login/filebase-google-login.component.ts
+++ src/app/filebase-google-login/filebase-google-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 import firebase from 'firebase/compat/app';
@@ -9,12 +9,16 @@ import { Observable } from 'rxjs';
   templateUrl: './filebase-google-login.component.html',
   styleUrls: ['./filebase-google-login.component.scss']
 })
-export class FilebaseGoogleLoginComponent implements OnInit {
+export class FilebaseGoogleLoginComponent implements OnInit, AfterViewInit {
 
   user$!: Observable<firebase.User | null>;
   user!: any;
 
   constructor(public auth: AngularFireAuth) {
+  }
+
+  ngAfterViewInit(): void {
+    GoogleAuth.initialize()
   }
 
   ngOnInit() {
@@ -24,8 +28,10 @@ export class FilebaseGoogleLoginComponent implements OnInit {
   login() {
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
+
   logout() {
     this.auth.signOut();
+    GoogleAuth.signOut();
   }
 
   loginAndroid() {
@@ -38,6 +44,14 @@ export class FilebaseGoogleLoginComponent implements OnInit {
     GoogleAuth.signOut()
     .then(v => this.user = null)
     .catch(console.log);
+  }
+
+  googleFirebaseLogin() {
+    GoogleAuth.signIn().then(user => {
+      let c = firebase.auth.GoogleAuthProvider
+                    .credential(user.authentication.idToken, user.authentication.accessToken);
+      this.auth.signInWithCredential(c);
+    });
   }
 
 }
